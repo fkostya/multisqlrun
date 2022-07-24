5bed6c9a1a7e867953b31bf774fda935f641553b
Froimovich, Konstantin <froimovich@gmail.com>
2022-07-24 13:26:39 -0400

    add unit test

--- appui.tests/LoadConnectionTests.cs
+++ appui.tests/LoadConnectionTests.cs
@@ -1,12 +1,33 @@
+using appui.shared;
 using appui.shared.Interfaces;
 using HtmlAgilityPack;
+using Microsoft.Extensions.Options;
+using Moq;
 using NUnit.Framework;
-using Telerik.JustMock;
 
 namespace appui.tests
 {
+    class PageReaderStub : IPageReader
+    {
+        public async Task<HtmlDocument> GetPageAsync()
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml("<html></html>");
+
+            return await Task.FromResult(doc);
+        }
+    }
+
+    class PageReaderFactoryStub : IPageReaderFactory
+    {
+        public IPageReader CreatePageReader()
+        {
+            return new PageReaderStub();
+        }
+    }
     //UnitIfWork_InitialCondition_ExpectedResult
     //Example: UserLogsIn_WithValidCredetntials_RedirectsToHome
+    [TestFixture]
     public class LoadConnectionTests
     {
         public LoadConnectionTests()
@@ -14,10 +35,19 @@ namespace appui.tests
 
         }
 
-        [Test]
-        [Category("Shared")]
+        [TestCase]
         public void UnitIfWork_InitialCondition_ExpectedResult()
         {
+            //Options.Create(new SampleOptions());
+
+            //var mock = new Mock<IPageReaderFactory>()
+            //    .Setup(c => c.CreatePageReader())
+            //    .Returns(new MyClass());
+
+            LoadConnections con= new LoadConnections(new PageReaderFactoryStub());
+            var doc = con.Load();
+            
+            Assert.IsNotNull(doc);
         }
     }
 }
