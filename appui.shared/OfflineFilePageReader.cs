f83810af45aff9054706667db9b4a2be3a6fd2e7
kostya <froimovich@gmail.com>
2022-08-12 17:19:29 -0400

    added unit test for OfflineFilePageReader class

--- appui.shared/OfflineFilePageReader.cs
+++ appui.shared/OfflineFilePageReader.cs
@@ -1,6 +1,7 @@
 ﻿using appui.shared.Interfaces;
 using appui.shared.Models;
 using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace appui.shared
@@ -9,20 +10,31 @@ namespace appui.shared
     {
         private readonly CatalogConnection config;
         private const string support_type = "df-offline";
+        private readonly ILogger logger;
 
-
-        public OfflineFilePageReader(IOptions<List<CatalogConnection>> options)
+        public OfflineFilePageReader(IOptions<List<CatalogConnection>> options, ILogger<AppErrorLog> logger)
         {
-            config = options?.Value?
+            this.config = options?.Value?
                .Where(f => f.Name == support_type)
                .FirstOrDefault();
+            this.logger = logger;
+
+            this.logger.LogInformation($"OfflineFilePageReader will load file from path: {this.config?.FilePath}");
         }
 
         public async Task<HtmlDocument> LoadPageAsync()
         {
             var doc = new HtmlDocument();
-            doc.Load(config.FilePath);
+            try
+            {
+                doc.Load(config.FilePath);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, ex.Message);
 
+                doc = new HtmlDocument();
+            }
             return await Task.FromResult(doc);
         }
     }
---
+++ appui.tests/OfflineFilePageReaderTests.cs
@@ -0,0 +1,79 @@
+﻿using appui.shared;
+using appui.shared.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace appui.tests
+{
+    public class OfflineFilePageReaderTests
+    {
+        private OfflineFilePageReader buildOfflineFilePageReader(IOptions<List<CatalogConnection>>? options)
+        {
+            var logger = new Mock<ILogger<AppErrorLog>>();
+
+            return new OfflineFilePageReader(null, logger.Object);
+        }
+
+        [Fact]
+        public void CreatingNewInstance_WhenNoArgumentsProvided_InstanceIsNotNull()
+        {
+            OfflineFilePageReader reader = buildOfflineFilePageReader(null);
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public void CreatingNewInstance_WhenConnectionTypeNotProvided_InstanceIsNotNull()
+        {
+            var options = Options.Create(new List<CatalogConnection>());
+
+            var reader = buildOfflineFilePageReader(options);
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public void CreatingNewInstance_WhenConnectionTypeNotSupported_InstanceIsNotNull()
+        {
+            var options = Options.Create(new List<CatalogConnection>() { new CatalogConnection { Name = "df-test" } });
+
+            var reader = buildOfflineFilePageReader(options);
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public void CreatingNewInstance_WhenConnectionTypeSupported_InstanceIsNotNull()
+        {
+            var options = Options.Create(new List<CatalogConnection>() { new CatalogConnection { Name = "df-offline" } });
+
+            var reader = buildOfflineFilePageReader(options);
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public async Task LoadPageAsync_WhenUrlIsEmpty_DefaultDocumentIsNotNull()
+        {
+            var options = Options.Create(new List<CatalogConnection>() { new CatalogConnection { Name = "df-offline", FilePath= "" } });
+
+            var reader = buildOfflineFilePageReader(options);
+
+            var doc = await reader.LoadPageAsync();
+            Assert.NotNull(doc);
+        }
+
+        [Fact]
+        public async Task LoadPageAsync_WhenWebPageIsNull_DefaultDocumentIsNotNull()
+        {
+            var options = Options.Create(new List<CatalogConnection>() { new CatalogConnection { Name = "df-offline", FilePath = "" } });
+
+            var reader = buildOfflineFilePageReader(options);
+
+            var doc = await reader.LoadPageAsync();
+            Assert.NotNull(doc);
+        }
+    }
+}
