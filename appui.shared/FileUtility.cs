1d85f86eaa0330e96f46e6ab9bda85282f2f6bd5
kostya <froimovich@gmail.com>
2022-10-10 13:26:30 -0400

    added logger to file utility, updated unit tests

--- appui.shared/FileUtility.cs
+++ appui.shared/FileUtility.cs
@@ -1,19 +1,32 @@
 ﻿using appui.shared.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace appui.shared
 {
     public class FileUtility : IStorageUtility
     {
         private readonly IDirectoryWrapper _directoryWrapper;
+        private readonly ILogger _logger;
 
-        public FileUtility(IDirectoryWrapper directoryWrapper)
+        public FileUtility(IDirectoryWrapper directoryWrapper, ILogger<FileUtility> logger)
         {
             this._directoryWrapper = directoryWrapper;
+            this._logger = logger;
         }
 
         public DirectoryInfo CreateStorage(string path)
         {
-            return _directoryWrapper.CreateDirectory(path);
+            try
+            {
+                var directory = _directoryWrapper.CreateDirectory(path);
+                this._logger.LogTrace($"Cretated directory: {directory}");
+                return directory;
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError($"Error: {ex}");
+                throw;
+            }
         }
 
         public string GenerateUniqueStorageName(string rootStorageName)
